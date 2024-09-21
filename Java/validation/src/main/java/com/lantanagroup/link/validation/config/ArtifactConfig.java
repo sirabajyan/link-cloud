4c20e6808d2abfd0408dbcee1c59f566be2d8d3d
Steven Williams <steven.williams@lantanagroup.com>
2024-09-21 16:43:45 -0400

    Reconcile default value in code/configuration

--- Java/validation/src/main/java/com/lantanagroup/link/validation/config/ArtifactConfig.java
+++ Java/validation/src/main/java/com/lantanagroup/link/validation/config/ArtifactConfig.java
@@ -8,5 +8,5 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 @Getter
 @Setter
 public class ArtifactConfig {
-    private boolean init = true;
+    private boolean init = false;
 }
