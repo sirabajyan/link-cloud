aaa7533d262d3a30573a665e6dc4dcacc4bd3bae
edward-miller-lcg <119338797+edward-miller-lcg@users.noreply.github.com>
2024-11-01 15:31:20 -0500

    LNK-3045: QD Mass Assignment RELEASE

--- DotNet/QueryDispatch/Application/Models/DataAcquisitionRequestedValue.cs
+++ DotNet/QueryDispatch/Application/Models/DataAcquisitionRequestedValue.cs
@@ -1,9 +1,15 @@
-﻿namespace LantanaGroup.Link.QueryDispatch.Application.Models
+﻿using System.Runtime.Serialization;
+
+namespace LantanaGroup.Link.QueryDispatch.Application.Models
 {
+    [DataContract]
     public class DataAcquisitionRequestedValue
     {
+        [DataMember]
         public string PatientId { get; set; } = String.Empty;
+        [DataMember]
         public List<ScheduledReport> ScheduledReports { get; set; } = new List<ScheduledReport>();
+        [DataMember]
         public string QueryType { get; set; } = QueryTypes.Initial.ToString();
     }
 }
