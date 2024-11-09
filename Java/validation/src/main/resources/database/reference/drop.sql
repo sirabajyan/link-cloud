dd89b1a968274836f7d58f8948b1e8d6183dcc3f
Steven Williams <steven.williams@lantanagroup.com>
2024-11-09 12:20:53 -0500

    Generate both create and drop scripts

---
+++ Java/validation/src/main/resources/database/reference/drop.sql
@@ -0,0 +1,11 @@
+
+    alter table category_rule 
+       drop constraint fk_category_rule_category_id;
+
+    drop table artifact;
+
+    drop table category;
+
+    drop table category_rule;
+
+    drop table result;
